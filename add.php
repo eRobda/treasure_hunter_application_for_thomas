d4e1f7c2c50c5276b76e095033c7a3a92ee42ca3
Robin Ustohal <Ustik96@gmail.com>
2025-01-27 11:05:13 +0100

    add: loading to navbar

--- add.php
+++ add.php
@@ -107,7 +107,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     </form>
 
     <div class="btm-nav">
-        <a href="index.php">
+        <a href="index.php" onclick="OnLoading()">
             <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24"
                 stroke="currentColor">
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
